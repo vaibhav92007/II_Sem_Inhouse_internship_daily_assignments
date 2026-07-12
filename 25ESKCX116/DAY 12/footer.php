a64a3f93ef51e5a25772a2d3f43bc243f96c8d0b
Vaibhav Bhardwaj <270537096+vaibhav92007@users.noreply.github.com>
2026-07-12 15:17:06 +0530

    Add footer to the footer.php file

---
+++ 25ESKCX116/DAY 12/footer.php
@@ -0,0 +1,9 @@
+<footer class ="bg-dark text-white text-centre py-3 mt-5">
+    <p class ="mb-0">&copy;<?php echo date("Y");?>My Website.All Rights Reserved.</p>
+</footer>
+
+
+<!--bootstrap js-->
+<script src ="bootstrap.bundle.min.js"></script>
+</body>
+</html>
