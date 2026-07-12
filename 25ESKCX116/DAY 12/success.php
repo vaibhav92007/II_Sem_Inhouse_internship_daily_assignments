908e9ca5b17670789bd4414529f0183f93d50be2
Vaibhav Bhardwaj <270537096+vaibhav92007@users.noreply.github.com>
2026-07-12 15:24:20 +0530

    Add success page for registration confirmation

---
+++ 25ESKCX116/DAY 12/success.php
@@ -0,0 +1,22 @@
+<?php
+include ("header.php");
+?>
+<div class="container mt-5
+text-center">
+<div class="alert alert-success">
+    <h3>
+        Registration Successful!
+    </h3>
+    <p> your form has been submitted successfully.</p>
+</div>
+<a href="registration.php"
+class="btn btn-primary">
+Go Back</a>
+</div>
+
+
+
+
+<?php
+include ("footer.php");
+?>
