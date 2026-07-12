a79e60e4f72c1e5b5878794ef80985926ef9d5b2
Vaibhav Bhardwaj <270537096+vaibhav92007@users.noreply.github.com>
2026-07-12 14:33:35 +0530

    Add test.php with age check and print statements

---
+++ 25ESKCX116/DAY 7/test.php
@@ -0,0 +1,18 @@
+
+<?php
+echo "hi <br>" , "KUCH BHI <br>";
+print("hi <br>");
+print "hi <br>";
+$age=20;
+if($age>=18){
+    echo "ADULT" ;
+}else{
+    echo "NOT adult";
+}
+$ret=($age>=18)? print("<br>adult"):print("<br>not adult");
+
+if(print("<br>true")){
+   print("<br>false<br>");
+}
+
+?>
