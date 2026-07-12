9dd94ba18266fabcc8305ad326d818122585967a
Vaibhav Bhardwaj <270537096+vaibhav92007@users.noreply.github.com>
2026-07-12 14:31:23 +0530

    Create registration.php for user data submission
    Add registration form handling in registration.php

---
+++ 25ESKCX116/DAY 7/registration.php
@@ -0,0 +1,17 @@
+<?php
+$name=$_POST["name"];
+$email=$_POST["email"];
+$phoneno=$_POST["phone_number"];
+$date=$_POST["dob"];
+$gender=$_POST["gender"];
+$address=$_POST["address"];
+$city=$_POST["city"];
+$pin=$_POST["pin"];
+$state=$_POST["state"];
+$error[]=[];
+
+
+
+
+echo "$name <br>$email<br>$phoneno<br>$date<br>$gender<br>$address<br>$state<br>$city<br>$pin ";
+?>
