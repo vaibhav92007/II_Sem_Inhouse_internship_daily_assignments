1d3062dd9c1b4931305752aca53d9304cfd846fc
Vaibhav Bhardwaj <270537096+vaibhav92007@users.noreply.github.com>
2026-07-12 14:28:37 +0530

    Add registration form in form.php

---
+++ 25ESKCX116/DAY 7/form.php
@@ -0,0 +1,11 @@
+<html>
+    <form action="registration.php" method="post">
+        <input type="text" name="name" placeholder="Enter your name" required/>
+        <input type="email" name="email" placeholder="Enter your email" required/>
+        <input type="text" name="branch" placeholder="Enter your branch" required/>
+        <input type="text" name="phoneno" placeholder="Enter your phone number" required />
+        <button type="submit">
+            REGISTER
+        </button>
+    </form>
+</html>
